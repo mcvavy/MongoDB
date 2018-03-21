92fcb3f40d882a40d5081feeeea6fce08f0b9f46
Michael Oyibo <codingrealms@gmail.com>
2018-03-21 04:50:11 +0000

    Instantiating and connecting to MongoDB

--- test/connection.js
+++ test/connection.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 //Connect to MongoDB
 mongoose.connect('mongodb://localhost/testdb');
 
-mongoose.connection.once('open', () => console.log("Connection successful......."))
+mongoose.connection
+.once('open', () => console.log("Connection successful......."))
 .on('error', (err) => console.log('connection error', err));
 
